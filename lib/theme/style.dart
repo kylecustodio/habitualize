a745b7cda08433dc0f450c3bc020dbcb86bde971
Kyle Custodio <kkzyc@yahoo.com>
2020-09-09 10:43:45 -0500

    add Quicksand font

--- lib/theme/style.dart
+++ lib/theme/style.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 
 ThemeData appTheme() {
   return ThemeData(
-    primarySwatch: Colors.blue,
+    primaryColor: Colors.teal[100],
+    accentColor: Colors.red[100],
+
+    fontFamily: 'Quicksand',
+    
+    appBarTheme: AppBarTheme(
+      elevation: 0.0,
+    ),
   );
 }
