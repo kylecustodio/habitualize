d4241f1668f8d8d813fc6cc6985d4cf8799f1f1b
Kyle Custodio <kkzyc@yahoo.com>
2020-11-24 12:01:32 -0600

    get new habit to home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:habitualize/models/habit.dart';
 import 'package:habitualize/screens/habitForm.dart';
 
 class Home extends StatefulWidget {
@@ -7,6 +8,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final _habits = List<Habit>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,12 +18,13 @@ class _HomeState extends State<Home> {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
+        onPressed: () async {
           // navigate to new habit form
-          Navigator.push(
+          Habit newHabit = await Navigator.push(
             context,
             MaterialPageRoute(builder: (context) => HabitForm())
           );
+          _habits.add(newHabit);
         },
       ),
     );
