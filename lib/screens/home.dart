7575ca730103b4b2a6651872c0af9fe2b625b809
Kyle Custodio <kkzyc@yahoo.com>
2021-02-23 11:10:45 -0600

    add home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:habitualize/models/habit.dart';
 import 'package:habitualize/screens/habitForm.dart';
+import 'package:habitualize/widgets/rowBuilder.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,23 +9,43 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final _habits = List<Habit>();
+  List<Habit> _habits = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Habitualize')
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Center(child: Text('Habits', style: TextStyle(fontSize: 32))),
+          SizedBox(height: 20),
+          _habits.length == 0
+              ? Text('Add a habit')
+              : Expanded(
+                  child: ListView.builder(
+                    itemCount: _habits.length,
+                    itemBuilder: (context, index) =>
+                        RowBuilder(habit: _habits[index]),
+                  ),
+                )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.add),
+        label: Text('Add Habit'),
+        elevation: 0,
+        focusElevation: 0,
+        highlightElevation: 0,
+        hoverElevation: 0,
         onPressed: () async {
           // navigate to new habit form
+          // returns new habit
           Habit newHabit = await Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => HabitForm())
-          );
-          _habits.add(newHabit);
+              context, MaterialPageRoute(builder: (context) => HabitForm()));
+          setState(() {
+            _habits.add(newHabit);
+          });
         },
       ),
     );
