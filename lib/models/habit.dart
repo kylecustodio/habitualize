51b7a4f684fe561555adcfe2b3280b6bcaff9702
Kyle Custodio <kkzyc@yahoo.com>
2021-02-24 21:33:09 -0600

    strip down habit for MVC

--- lib/models/habit.dart
+++ lib/models/habit.dart
@@ -1,18 +1,21 @@
-enum HabitType {
-  buildHabit,
-  breakHabit
-}
+// enum HabitType {
+//   buildHabit,
+//   breakHabit
+// }
 
-enum GoalType {
-  simple,
-  counter,
-}
+// enum GoalType {
+//   simple,
+//   counter,
+// }
 
 class Habit {
   String name;
-  HabitType habitType;
-  GoalType goalType;
-  int goalNum;
+  bool isDone = false;
+  // HabitType habitType;
+  // GoalType goalType;
+  // int goalNum;
 
-  Habit({this.name, this.habitType, this.goalType, this.goalNum});
+  // Habit({this.name, this.habitType, this.goalType, this.goalNum});
+  Habit({this.name});
+
 }
